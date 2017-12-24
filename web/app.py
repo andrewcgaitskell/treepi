fe84dadbcae0cadff79a9b99787170f69601a5dc
andrewcgaitskell <andrew@gaitskell.com>
2017-12-24 16:05:16 +0000

    Create app.py

---
+++ web/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/hi/<name>")
+def say_hi(name):
+    return 'Hello, %s!' % (name)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
